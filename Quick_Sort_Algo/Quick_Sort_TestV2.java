a94e6e615f02b1ce51101dfa3bac9608109dd350
Aidan1246 <aidan.vincent@student.tdsb.on.ca>
2025-12-05 16:58:55 -0500

    Figuring out how to get rid of the 0's out of the array

---
+++ Quick_Sort_Algo/Quick_Sort_TestV2.java
@@ -0,0 +1,105 @@
+/**
+ * Write a description of class Quick_Sort_Test here.
+ *
+ * @ Aidan, Liam, and Lorne.
+ * @ Dec 2 - #, 2025.
+ */
+public class Quick_Sort_TestV2
+{
+    public static void main(String[] args) 
+    {
+        int[] list = {4,9,6,7,5,8,3,1,2};
+        int number = 0;
+        
+        quickSearch(list, number);
+    }
+    
+    public static int quickSearch(int[] list, int number)
+    {
+        number = list[list.length/2];
+        
+        // adds the amount for a list
+        int bigCount = 0;
+        int smallCount = 0;
+        
+        int[] big = new int[bigCount];
+        int[] small = new int[smallCount];
+        
+        int bigger = 0;
+        int smaller = 0;
+         
+        for(int i = 0; i < list.length; i++)
+        {
+            //System.out.print(list[i]);
+            if(list[i] > number)
+            {
+                //System.out.println(list[i]+" is Bigger");
+                //big[bigCount] = list[i];
+                
+                // System.out.println(big[i]);
+                // System.out.println("Index " + i);
+                
+                bigCount--;
+            }
+            else if(list[i] == number)
+            {
+                System.out.println(list[i]+" is Equal");
+            }
+            else if (list[i] < number)
+            {
+                //System.out.println(list[i]+" is Smaller");
+                //small[smallCount] = list[i];
+                
+                // System.out.println(small[i]);
+                // System.out.println("Index " + i);
+                
+                smallCount++;
+            }
+        }
+        
+        for(int i = 0; i < list.length; i++)
+        {
+            if(list[i] > number)
+            {
+                //System.out.println(list[i]+" is Bigger");
+                //big[bigCount] = list[i];
+                
+                // System.out.println(big[i]);
+                // System.out.println("Index " + i);
+                
+                bigCount--;
+            }
+            else if (list[i] < number)
+            {
+                //System.out.println(list[i]+" is Smaller");
+                //small[smallCount] = list[i];
+                
+                // System.out.println(small[i]);
+                // System.out.println("Index " + i);
+                
+                smallCount++;
+            }
+        }
+        
+        /*
+        System.out.println(" ");
+        
+        for(int i = 0; i < small.length; i++)
+        {
+            System.out.print(small[i]); // prints 400000312 
+            // (Numbers are located based off of original index, meaning 0 is just a filler)
+        }
+        
+        System.out.println(" ");
+        
+        for(int i = 0; i < big.length; i++)
+        {
+            System.out.print(big[i]); // prints 096708000 
+            // (Numbers are located based off of original index, meaning 0 is just a filler)
+        }
+
+        System.out.println(" ");
+        */
+        return number;
+    }
+}
